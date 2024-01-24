a37b260f40e1c943cd342e4cde6f6d18782b4c33
Cameron McGlasham <mcglasham.cameron@outlook.com>
2024-01-24 12:52:25 +0000

    Initial Commit

---
+++ What-Do?/What_Do_App.swift
@@ -0,0 +1,17 @@
+//
+//  What_Do_App.swift
+//  What-Do?
+//
+//  Created by Cameron McGlasham on 24/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct What_Do_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
